3a2118a9c8f1697f6509dec06002703d19231bf4
Your Name <aqatra6@gmail.com>
2024-08-22 12:32:23 +0300

    complete lab

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,39 @@
+# Function to create an empty list
 def create_an_empty_list():
-    return None
+    return []
 
+# Function to create a list with four elements
 def create_a_list():
-    return None
+    return [1, 2, 3, 4]
 
-def add_element_to_end_of_list(l, element):
-    return None
+# Function to add an element to the end of a list
+def add_element_to_end_of_list(lst, element):
+    lst.append(element)
+    return lst
 
-def add_element_to_start_of_list(l, element):
-    return None
+# Function to add an element to the start of a list
+def add_element_to_start_of_list(lst, element):
+    lst.insert(0, element)
+    return lst
 
-def remove_element_from_end_of_list(l):
-    return None
+# Function to remove the last element from a list
+def remove_element_from_end_of_list(lst):
+    lst.pop()
+    return lst
 
-def remove_element_from_start_of_list(l):
-    return None
+# Function to remove the first element from a list
+def remove_element_from_start_of_list(lst):
+    del lst[0]
+    return lst
 
-def retrieve_first_element_from_list(l):
-    return None
+# Function to retrieve the first element from a list
+def retrieve_first_element_from_list(lst):
+    return lst[0]
 
-def retrieve_element_from_index(l, index):
-    return None
+# Function to retrieve an element from a specific index in a list
+def retrieve_element_from_index(lst, index):
+    return lst[index]
 
-def retrieve_last_element_from_list(l):
-    return None
+# Function to retrieve the last element from a list
+def retrieve_last_element_from_list(lst):
+    return lst[-1]
